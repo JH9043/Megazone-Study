a874909d6555f68cb03bb7f4c4eb1d09f9a1db63
JH9043 <whdgkr9070@naver.com>
2022-08-08 09:29:58 +0900

    Create Project

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+
+public class Student {
+	private String hakbun, name;
+	private int kor, eng, mat, edp, tot;
+	private double avg;
+	private char grade;
+	public Student() { //Default Constructor 기본 생성자
+		
+	}
+}
