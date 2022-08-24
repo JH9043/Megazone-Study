9f0e172cfe0f3c2ea53df1cf28fbb26d717f5513
JH9043 <whdgkr9070@naver.com>
2022-08-24 17:54:33 +0900

    0824

--- src/main/webapp/js/scoreData.js
+++ src/main/webapp/js/scoreData.js
@@ -29,16 +29,61 @@ function processJSCSV() {
 				var total_1 = total.split(",");
 				document.getElementById("40").innerHTML = total_1[0];
 				document.getElementById("41").innerHTML = total_1[1];
+				
+				for (i = 0; i < result.length; i++) {
+					if (i % 3 == 1) { //1, 4, 7
+						sum = eval(result[i] + "+" + result[i + 1]);
+						//13 23 33
+						document.getElementById((i+1)+"3").innerHTML = sum;
+						document.getElementById((i+1)+"4").innerHTML = sum / 2;
+						id += 10;
+					}
 
+
+				}
+				a = document.getElementById(13).innerHTML;
+				b = document.getElementById(23).innerHTML;
+				c = document.getElementById(33).innerHTML;
+				array_num = [3, 2, 1];
+				sort = [a, b, c];
+				sort1 = sort.sort();
+				id_sort = 13;
+				console.log(sort1);
+				for (i = 0; i < 3; i++) {
+					for (j = 0; j < sort1.length; j++)
+						if (document.getElementById("" + id_sort).innerHTML == sort1[j])
+							document.getElementById("" + (id_sort + 2)).innerHTML = array_num[j];
+					id_sort += 10;
+
+				}
+
+
+
+				//				for (i = 0; i < 3; i++) {
+				//					for (j = 0; j < ; j++) {
+				//						var itemValue = result[num1];
+				//						var str = String(i + 1);
+				//						var str1 = String(j);
+				//						var str2 = str + str1;
+				//						if (str2 % 10 == 3) {
+				//							document.getElementById(str2).innerHTML = itemValue;
+				//
+				//						}
+				//					}
+				//
+				//				}
+				//				document.getElementById("13").innerHTML = result
+				//13 23 33            //1,2 4,5 7,8
 			}
 		}
 		xhr.send();
 	}
 }
 
+
 function getTotalJSCSV(responseText) {
 	if (xhr != null) {
-		xhr.open("GET", "/WEBTest/jsp/scoreDataProcess.jsp?responseText="+responseText, false); //open(전송 방식, URL, 동기(false) 비동기(true))
+		xhr.open("GET", "/WEBTest/jsp/scoreDataProcess.jsp?responseText=" + responseText, true); //open(전송 방식, URL, 동기(false) 비동기(true))
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && xhr.status == 200) {
 				return total = xhr.responseText;
@@ -68,5 +113,13 @@ function processJSXML() {
 			}
 		}
 		xhr.send();
+	}
+}
+
+function processINIT() {
+	var tdElements = document.getElementsByTagName("td");
+	var te = tdElements.length;
+	for (i = 0; 0 < te; i++) {
+		tdElements[i].innerHTML = "";
 	}
 }
--- src/main/webapp/jsp/scoreDataProcess.jsp
+++ src/main/webapp/jsp/scoreDataProcess.jsp
@@ -5,6 +5,20 @@
 	String st[] = responseText.split(",");
 	int engTotal = 0;
 	int mathTotal = 0;
+	//
+// 	int studentCount = 3;
+// 	int subjectCount = 2;
+// 	int[] studentsTotal = new int[studentsCount];
+// 	float[] studentsAvg = new float[studentsCount];
+// 	DecimalFormat df = new DecimalFormat("##.00");
+	
+// 	for (int i =0; i<strArr.length; i++){
+// 		if (i%studentsCount ==0){
+// 			studentsTotal[(int)i/studentsCount] = Integer.parseInt(strArr[i+1]) + Integer.parseInt(strArr[i+2]);
+// 			studentsAvg[(int)i/studentCount] = studentsTotal[(int)i/studentsCount] / subjectCount;
+// 			studentsAvgStr[(int)i/studentsCount] = df.format((double)(studentsAvg[(int)i/studentsCount]));
+// 		}
+// 	}
 	
 	for (int i = 0; i<st.length; i++){
 		if (i%3==1){
@@ -14,6 +28,15 @@
 			mathTotal += Integer.parseInt(st[i]);
 		}
 	}
+	
+// 	for (int i = 0; i<st.length; i++){
+// 		if (i%3==1){
+// 			subTotal = Integer.parseInt(st[i]) + Integer.parseInt(st[i+1])
+		
+// 		}
+// 	}
+	
+	
 	out.print(engTotal+","+mathTotal);
 
 
