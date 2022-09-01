45cb0f6f37322c4596a7172b83310884d2978348
JH9043 <whdgkr9070@naver.com>
2022-09-01 17:58:44 +0900

    0901

--- src/main/webapp/js/race.js
+++ src/main/webapp/js/race.js
@@ -12,41 +12,52 @@ function button() {
 		var id2 = "img" + i;
 		var name = "move" + i;
 		var second = random + "s";
-
+		var timeran = (String)(Math.floor(Math.random() * (600 - 1 + 1)) + 1);
 
 		document.getElementById(id2).style.animationName = name;
 		document.getElementById(id2).style.animationDuration = second;
-		
+
 	}
 
-	setInterval(function() {
-		time++;
+	clock = setInterval(function() {
+		        time++; // 1초마다 증가, 타이머의 경우 time--;
 
-		min = Math.floor(time / 60);
-		hour = Math.floor(min / 60);
-		sec = time % 60;
-		min = min % 60;
+        min = Math.floor(time/60);
+        hour = Math.floor(min/60);
+        sec = time%60;
+        min = min%60;
 
-		var th = hour;
-		var tm = min;
-		var ts = sec;
-		if (th < 10) {
-			th = "0" + hour;
-		}
-		if (tm < 10) {
-			tm = "0" + min;
-		}
-		if (ts < 10) {
-			ts = "0" + sec;
-		}
+        var th = hour;
+        var tm = min;
+        var ts = sec;
+        
+        // 한자리일 경우 처리
+        if(th < 10){
+            th = "0" + hour;
+        }
+        if(tm < 10){
+            tm = "0" + min;
+        }
+        if(ts < 10){
+            ts = "0" + sec;
+        }
 
 		for (i = 1; i < 5; i++) {
 			var id = "time" + i;
 			var timer = (th + ":" + tm + ":" + ts);
+			var html_1 = document.getElementById(id).innerHTML;
 			document.getElementById(id).innerHTML = timer;
+			console.log(html_1)
+//			if (tm == random) {
+//				html_1 = timer;
+//			}
+
 		}
 
+
 	})
+
+
 }
 
 
